a1e0805fad58922582a3653f5abe5c8ee3a7a3a3
thiendoan <doanchithien1118@gmail.com>
2021-11-02 15:00:46 +0700

    validate form

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
     has_one_attached :image
-    validates :publisher, numericality: false
+    validates :name, :description, :published_date, :publisher, :borrow_fee,:quantity,:quantity_in_stock , presence: true
 end
