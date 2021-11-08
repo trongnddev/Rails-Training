25a991f353949e779ed2d7bf7c9ec079eb59b2ef
thiendoan <doanchithien1118@gmail.com>
2021-11-08 17:04:20 +0700

    [thienDC] delete folder test

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :authors
   resources :books
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+  get "/about" ,to: "home#about"
 end
