c2b51bff9214717ebb9077a68f85a684581f07ee
thiendoan <doanchithien1118@gmail.com>
2021-11-16 16:21:58 +0700

    [thienDC] feature find book by name or author name

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,5 +3,5 @@ class Author < ApplicationRecord
   has_many :books, through: :author_books
   accepts_nested_attributes_for :author_books
   validates_format_of :author_name, with: /\A[[:alpha:][:blank:]]+\z/
-
+  before_create { |author| author.author_name= author.author_name.capitalize}
 end
