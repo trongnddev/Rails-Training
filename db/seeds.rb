5547b2ad95ff1b42f8a6833de6cda054d8ab319f
DatPT <phamdat.baron@gmail.com>
2021-11-23 10:46:28 +0700

    [DatPT] CRUD users for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
+User.create(email: "admin@email.com", password: "111111", password_confirmation: "111111")
 
+for i in 1..50
+    User.create(email: "user_#{i}@email.com", password: "111111", password_confirmation: "111111")
+    puts "User #{i} created"
+end
