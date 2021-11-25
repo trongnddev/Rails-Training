078d631ad7c1a6e3696be5e1dbc94f68676ea5be
trongnd <nguyendotrong.58@gmail.com>
2021-11-25 16:27:23 +0700

    [borrow book] update status, update quantity in stock, update penalty fee when user return book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_070617) do
+ActiveRecord::Schema.define(version: 2021_11_25_045119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 2021_11_19_070617) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.bigint "book_id"
-    t.integer "penalty_fee"
+    t.float "penalty_fee"
     t.index ["book_id"], name: "index_borrows_on_book_id"
     t.index ["user_id"], name: "index_borrows_on_user_id"
   end
