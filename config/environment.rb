aa7e9d32299b05884f1a4363d13fc3bd346cb306
thiendoan <doanchithien1118@gmail.com>
2021-12-17 09:54:37 +0700

    [ThienDC] user recoverble,custom css form

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true 
+ActionMailer::Base.smtp_settings = {
+    address:                 'smtp.gmail.com',
+    port:                    '465',
+    domain:                  'gmail.com',
+    user_name:               'thiendoan0811@gmail.com',
+    password:                'thuyngan0811',
+    authentication:          'plain',
+    :ssl                   => true,
+    :tsl                   => true,
+    enable_starttls_auto:     true
+}
